b6978c908a5b2ff3dd3fabaabfce6fe8bc317ea9
Dinesh Reddy <akkala420@gmail.com>
2020-11-19 21:44:43 -0500

    updated moviesAdpater

--- app/src/main/java/com/example/custome_list_view/adpaters/MoviesCustomAdpater.java
+++ app/src/main/java/com/example/custome_list_view/adpaters/MoviesCustomAdpater.java
@@ -3,9 +3,22 @@ package com.example.custome_list_view.adpaters;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.custome_list_view.Models.Movies;
+import com.example.custome_list_view.R;
+
+import java.util.ArrayList;
 
 public class MoviesCustomAdpater extends BaseAdapter {
 
+    ArrayList<Movies> movies;
+
+    public MoviesCustomAdpater(ArrayList<Movies> movies) {
+        this.movies=movies;
+
+    }
     @Override
     public int getCount() {
         return 0;
@@ -24,5 +37,21 @@ public class MoviesCustomAdpater extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         return null;
+    }
+
+    class ViewHolder{
+
+        TextView moviesNameTV;
+        TextView RatingTV;
+        TextView DescriptionTV;
+        ImageView ImageTV;
+        public  ViewHolder(View view)
+        {
+            moviesNameTV=view.findViewById(R.id.MovieName);
+            RatingTV=view.findViewById(R.id.ratings);
+            DescriptionTV=view.findViewById(R.id.Description);
+            ImageTV=view.findViewById(R.id.image_view);
+
+        }
     }
 }
