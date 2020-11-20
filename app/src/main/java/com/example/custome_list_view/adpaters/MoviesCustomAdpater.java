bf944d5417d44cb42c0bef66ed7d5b8d7869a9dd
Dinesh Reddy <akkala420@gmail.com>
2020-11-19 21:39:26 -0500

    extended MoviesCustomAdpater with baseadpater and implemented nmethods

--- app/src/main/java/com/example/custome_list_view/adpaters/MoviesCustomAdpater.java
+++ app/src/main/java/com/example/custome_list_view/adpaters/MoviesCustomAdpater.java
@@ -1,8 +1,28 @@
 package com.example.custome_list_view.adpaters;
 
-public class MoviesCustomAdpater {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
-    public MoviesCustomAdpater() {
+public class MoviesCustomAdpater extends BaseAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
     }
 
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
